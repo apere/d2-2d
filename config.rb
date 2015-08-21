28082d0193aa389d0631133911d1fa9c3c53bcd2
adam m pere <adampere001@gmail.com>
2015-08-21 08:25:20 -0400

    a bit of reorginizing, about page

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css"
-sass_dir = "sass"
+sass_dir = "develop/sass"
 images_dir = "assets/imgs"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
